ecfa5e6d5454b3d65673adbb0854a5b0d96767ec
Camilo Nossa <alejandro_noss@hotmail.com>
2023-04-02 20:44:44 -0500

    Added runnable method Actor for used in threads

--- server/src/main/java/com/server/logic/ActorRenewal.java
+++ server/src/main/java/com/server/logic/ActorRenewal.java
@@ -2,20 +2,23 @@ package com.server.logic;
 
 import com.server.model.Actor;
 import org.zeromq.ZMQ;
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 
-public class ActorRenewal implements Actor {
+public class ActorRenewal implements Actor, Runnable {
     @Override
     public void execute() {
-        String topic = "Renewal", port = "51151";
+        String topic = "Renewal", port = "5115";
 
-        try(ZContext context = new ZContext()) {
-            ZMQ.Socket subscriber = context.createSocket(ZMQ.SUB);
+        try (ZContext context = new ZContext()) {
+            ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
 
             subscriber.connect("tcp://localhost:" + port);
             subscriber.subscribe(topic.getBytes(ZMQ.CHARSET));
 
             while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("Thread " + topic + " is listening...");
+                String renewalTopic = subscriber.recvStr(0);
                 String message = new String(subscriber.recv(0), ZMQ.CHARSET);
 
                 /* Codigo */
@@ -25,8 +28,14 @@ public class ActorRenewal implements Actor {
             context.destroy();
             subscriber.close();
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Error: " + e.getMessage() + " The Actor:" + topic + " can't be executed.");
         }
+    }
+
+    @Override
+    public void run() {
+        execute();
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 }
--- server/src/main/java/com/server/logic/ActorReturn.java
+++ server/src/main/java/com/server/logic/ActorReturn.java
@@ -1,21 +1,28 @@
 package com.server.logic;
 
 import com.server.model.Actor;
+
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
-public class ActorReturn implements Actor {
+public class ActorReturn implements Actor, Runnable {
+
     @Override
     public void execute() {
-        String topic = "Return", port = "51151";
+        String topic = "Return", port = "5115";
 
-        try(ZContext context = new ZContext()) {
-            ZMQ.Socket subscriber = context.createSocket(ZMQ.SUB);
+        try (ZContext context = new ZContext()) {
+            ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
 
             subscriber.connect("tcp://localhost:" + port);
             subscriber.subscribe(topic.getBytes(ZMQ.CHARSET));
 
             while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("Thread " + topic + " is listening...");
+
+                String returnTopic = subscriber.recvStr(0);
+
                 String message = new String(subscriber.recv(0), ZMQ.CHARSET);
 
                 /* Codigo */
@@ -25,8 +32,14 @@ public class ActorReturn implements Actor {
             context.destroy();
             subscriber.close();
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Error: " + e.getMessage() + " The Actor:" + topic + " can't be executed.");
         }
+    }
+
+    @Override
+    public void run() {
+        execute();
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 }
