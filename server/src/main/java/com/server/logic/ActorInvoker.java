f3220da012ce7078c17f4f0f3e293763ba80169c
Camilo Nossa <alejandro_noss@hotmail.com>
2023-04-02 20:38:05 -0500

    Changed to static method

--- server/src/main/java/com/server/logic/ActorInvoker.java
+++ server/src/main/java/com/server/logic/ActorInvoker.java
@@ -3,7 +3,7 @@ package com.server.logic;
 import com.server.model.Actor;
 
 public class ActorInvoker {
-    public void invoke(Actor type) {
+    public static void invoke(Actor type) {
         new Thread(type::execute).start();
     }
 }
