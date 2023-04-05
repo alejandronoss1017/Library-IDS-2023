eb6ab828204ca616f87f7f7cb58ae880a2a652ec
Camilo Nossa <alejandro_noss@hotmail.com>
2023-04-04 22:27:51 -0500

    New client development

--- client/src/main/java/com/client/App.java
+++ client/src/main/java/com/client/App.java
@@ -5,29 +5,35 @@ import org.zeromq.ZMQ;
 import org.zeromq.ZContext;
 
 
-//
-//  Hello World client in Java
-//  Connects REQ socket to tcp://localhost:5555
-//  Sends "Hello" to server, expects "World" back
-//
 public class App {
     public static void main(String[] args) {
         try (ZContext context = new ZContext()) {
             // Socket to talk to server
-            System.out.println("Connecting to library server");
-
+            System.out.println("Connecting to server...");
             ZMQ.Socket socket = context.createSocket(SocketType.REQ);
-            socket.connect("tcp://localhost:5555");
+            socket.connect("tcp://10.43.100.59:5555");
 
+            System.out.println("Connection established");
+
+            // while (condition) {
+
+            // }
+
+            // Do 10 requests, waiting each time for a response
             for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
-                String request = String.valueOf(requestNbr);
-                System.out.println("Sending: " + requestNbr);
+
+                // Generate a random number between 1 and 2
+                int randomNumber = (int) (Math.random() * 2) + 1;
+
+                String request = Integer.toString(randomNumber);
+                System.out.println("Sending request " + requestNbr + "...");
                 socket.send(request.getBytes(ZMQ.CHARSET), 0);
 
                 byte[] reply = socket.recv(0);
-                System.out.println(
-                        "Received " + new String(reply, ZMQ.CHARSET) + " from library server");
+                System.out.println("Received reply " + requestNbr + " [" + new String(reply, ZMQ.CHARSET) + "]");
             }
+
         }
+
     }
 }
