a608a485a5958ffc502e919130c83b96a77cfc38
Camilo Nossa <alejandro_noss@hotmail.com>
2023-04-06 21:14:49 -0500

    Added a logger and refactorization for new load manager

--- server/src/main/java/com/server/App.java
+++ server/src/main/java/com/server/App.java
@@ -1,44 +1,20 @@
 package com.server;
 
-import com.server.logic.ActorManager;
-import com.server.logic.PublisherTest;
 import com.server.model.LoadManager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Initializing server...");
+        logger.info("Initializing server...");
 
         LoadManager loadManager = new LoadManager();
 
-        System.out.println("Server running on port 5555");
-
-        Thread loadManagerThread = new Thread(loadManager);
-
-        ActorManager.setUpActors();
-        PublisherTest publisher = new PublisherTest();
-        Thread publisherThread = new Thread(publisher);
-        publisherThread.start();
-
-        Thread.sleep(1000);
-
-        publisherThread.interrupt();
-        ActorManager.stopActors();
-
-        /*
-        // Applicant is not implemented yet
-        // final Actor actorApplicant = new ActorApplicant();
-        final ActorRenewal actorRenewal = new ActorRenewal();
-        final ActorReturn actorReturn = new ActorReturn();
-
-        // Thread actorApplicantThread = new Thread(actorApplicant);
-        Thread actorRenewalThread = new Thread(actorRenewal);
-        Thread actorReturnThread = new Thread(actorReturn);
-
-        // Threads are started
-        loadManagerThread.start();
-        actorRenewalThread.start();
-        actorReturnThread.start();
-         */
+        logger.info("Server running for Request-Reply on port 5555");
+        loadManager.receiveRequest();
     }
 }
