fda2e05ac699b3e5c17f757c73355c7268cdceb8
Camilo Nossa <alejandro_noss@hotmail.com>
2023-04-06 21:13:50 -0500

    Change to a simple class (No thread)

--- server/src/main/java/com/server/model/LoadManager.java
+++ server/src/main/java/com/server/model/LoadManager.java
@@ -4,72 +4,102 @@ package com.server.model;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
+
+import com.server.logic.ActorManager;
+import com.server.logic.Buffer;
+import com.server.logic.Publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.server.ServerInfo.*;
 
 /**
  * LoadManager
  */
-public class LoadManager implements Runnable {
+public class LoadManager {
 
     private Publisher publisher;
-    private ZMQ.Socket receiveSocket;
+    private ZMQ.Socket replySocket;
     private final ZContext context = new ZContext();
+
+    private static final Logger logger = LoggerFactory.getLogger(LoadManager.class);
 
     // Constructor for LoadManager
     public LoadManager() {
-        this.receiveSocket = context.createSocket(SocketType.REP);
-        this.publisher = new Publisher(context);
+        this.replySocket = context.createSocket(SocketType.REP);
+        this.publisher = new Publisher();
 
-        receiveSocket.bind("tcp://*:5555");
-
-        publisher.getPubSocket().bind("tcp://localhost:5556");
-        publisher.getRequestSocket().connect("tcp://localhost:5557");
-    }
-
-    @Override
-    public void run() {
-        try {
-            receiveRequest();
-            System.out.println("LoadManager");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        replySocket.bind("tcp://*:5555");
     }
 
     public void receiveRequest() throws InterruptedException {
 
-        String response = null;
-        String topic = null;
+        ActorManager.setUpActors();
+
+        Thread publisherThread = new Thread(publisher);
+        publisherThread.start();
+
+        ZMsg msg = new ZMsg();
 
         // Wait for next request from client
-        while (!Thread.currentThread().isInterrupted()) {
+        logger.debug("Waiting for request from client...");
+
+        Buffer buffer = Buffer.getInstance();
+
+        while (true) {
 
             // Receive request from client
-            byte[] request = receiveSocket.recv(0);
+            msg = ZMsg.recvMsg(replySocket);
 
             // Print the client request
-            System.out.println("Request from the client: " + new String(request, ZMQ.CHARSET));
+            System.out.println("================================================================");
+            System.out.println("Request received from the client: " + msg.toString());
 
-            if (new String(request, ZMQ.CHARSET).equals("1")) {
-                topic = "Return";
-                response = "Returning books";
+            if (msg.getFirst().toString().equalsIgnoreCase(RETURNTOPIC)) {
 
-                receiveSocket.send(response.getBytes(ZMQ.CHARSET), 0);
+                ZMsg msgToPub = new ZMsg();
 
-                System.out.println("Sending " + response + " to " + topic);
-                publisher.getPubSocket().send(topic);
+                msgToPub.add(RETURNTOPIC);
+                msgToPub.add(msg.getLast().toString());
 
-            } else if (new String(request, ZMQ.CHARSET).equals("2")) {
-                topic = "Renewal";
-                response = "Renewing books";
+                System.out.println("Sending return request to publisher " + msg.toString());
 
-                receiveSocket.send(response.getBytes(ZMQ.CHARSET), 0);
+                // Add message to buffer to be sent to publisher
+                buffer.produce(msgToPub);
 
-                System.out.println("Sending " + response + " to " + topic);
-                publisher.getPubSocket().send(topic);
+                // Send reply back to client
+                msg.clear();
+                msg.add("OK");
+                System.out.println("Sending reply back to client " + msg.toString());
+                System.out.println("================================================================");
+
+                msg.send(replySocket);
+                msg.clear();
+            } else if (msg.getFirst().toString().equalsIgnoreCase(RENEWALTOPIC)) {
+
+                System.out.println("Sending renewal request to publisher " + msg.toString());
+
+                ZMsg msgToPub = new ZMsg();
+
+                msgToPub.add(RENEWALTOPIC);
+                msgToPub.add(msg.getLast().toString());
+
+                System.out.println("Sending return request to publisher " + msg.toString());
+
+                // Add message to buffer to be sent to publisher
+                buffer.produce(msgToPub);
+
+                // Send reply back to client
+                msg.clear();
+                msg.add("OK");
+                System.out.println("Sending reply back to client " + msg.toString());
+                System.out.println("================================================================");
+
+                msg.send(replySocket);
+                msg.clear();
             }
-
-            // Wait for 1 second
-            Thread.sleep(1000);
         }
     }
 
