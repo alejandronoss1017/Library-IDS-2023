64a94b877255007e5c54ead164821ada3b745be5
jesusdme <jdmolinae@hotmail.com>
2023-04-07 02:46:50 -0500

    request-reply between client and server

--- client/src/main/java/logic/menu.java
+++ client/src/main/java/logic/menu.java
@@ -7,57 +7,38 @@ public class menu {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int option = -1;
-        while (option != 0) {
-            System.out.println("================================================================");
-            System.out.println("1. Commands explanation");
-            System.out.println("2. Enter command");
-            System.out.println("0. End execution");
 
-            System.out.print("Choose an option: ");
-            option = scanner.nextInt();
 
-            switch (option) {
-                case 1:
-                    System.out.println("============= Renew book =============");
-                    System.out.println("renew + <code of the receipt>");
-                    System.out.println(" ");
-
-                    System.out.println("============= Return book =============");
-                    System.out.println("return + <code of the receipt>");
-                    System.out.println(" ");
-
-                    System.out.println("============= Request book ============");
-                    System.out.println("request + <code of the book>");
-                    System.out.println(" ");
-                    break;
-
-                case 2:
-                    System.out.println("Enter the command: ");
                     /*Verification that the command is written in the right way*/
                     boolean continuar=false;//para preguntar otra vez si esta mal el comando escrito
                     while (!continuar) {
+                        System.out.println("Enter the command: ");
+
                         String command = scanner.nextLine();
 
                         if (command.startsWith("renew") || command.startsWith("request") || command.startsWith("return")) {
                             lastCommand = command; // actualiza el último comando ingresado por el usuario
                             continuar=true;
-                        } else {
-                            System.out.println("Enter one of the follow commands: renew, request, return.");
+                        }
+
+                        if (command.equals("help")){
+                            System.out.println("================================================================");
+                            System.out.println("============= Renew book =============");
+                            System.out.println("renew + <code of the receipt>");
+                            System.out.println(" ");
+
+                            System.out.println("============= Return book =============");
+                            System.out.println("return + <code of the receipt>");
+                            System.out.println(" ");
+
+                            System.out.println("============= Request book ============");
+                            System.out.println("request + <code of the book>");
+                            System.out.println(" ");
                         }
                     }
-                    break;
 
-                case 0:
-                    System.out.println("Thanks for use our system...");
-                    break;
-
-                default:
-                    System.out.println("Option no valid");
-                    break;
-            }
-        }
         scanner.close();
+
     }
 
     public static String getLastCommand() {
