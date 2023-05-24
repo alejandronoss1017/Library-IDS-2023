6670731e5164265cdb2d814677cd0db4b621110b
Camilo Nossa <calejandro_noss@outlook.com>
2023-05-23 19:35:34 -0500

    Re-factorization of App.java
    Console parameters added, implements flags to the parameters and better structure than before.

--- client/src/main/java/com/client/App.java
+++ client/src/main/java/com/client/App.java
@@ -1,69 +1,103 @@
 package com.client;
 
-import logic.Menu;
-
-import java.util.Scanner;
-
-import org.zeromq.SocketType;
-import org.zeromq.ZContext;
-import org.zeromq.ZMQ;
-import org.zeromq.ZMsg;
+import com.client.logic.Operation;
+import com.client.model.Menu;
 
 public class App {
     public static void main(String[] args) {
-        try (ZContext context = new ZContext()) {
-            // Socket to talk to server
-            System.out.println("Connecting to server...");
-            ZMQ.Socket requester = context.createSocket(SocketType.REQ);
-            boolean conecto = requester.connect("tcp://localhost:5555");
 
-            Scanner scanner = new Scanner(System.in);
-            Menu menu = new Menu();
-            ZMsg msg = new ZMsg();
-            while (conecto) {
+        String service = "";
+        String isbn = "";
+        int campus = 0;
+        String fileName = "";
 
-                menu.askForCommand();
-                menu.setLastCommand(scanner.nextLine());
-
-                // Send requests to server
-                String[] requests = (menu.getLastCommand()).split(" ");
-
-                for (String string : requests) {
-                    msg.add(string);
-                }
-
-                if ((menu.getLastCommand()).startsWith("Request")) {
-                    // hacer la accion de request
-                    System.out.println("Sending request: " + requests[1]);
-
-                    msg.send(requester);
-
-                    // Wait for response from server
-                    String reply = requester.recvStr(0).trim();
-                    System.out.println("Received reply: " + reply);
-                    msg.clear();
-                } else if ((menu.getLastCommand()).startsWith("Return")) {
-                    // hacer la accion de request
-                    System.out.println("Sending request: " + requests[1]);
-
-                    msg.send(requester);
-
-                    // Wait for response from server
-                    String reply = requester.recvStr(0).trim();
-                    System.out.println("Received reply: " + reply);
-                } else if ((menu.getLastCommand()).startsWith("Renewal")) {
-                    // hacer la accion de request
-                    System.out.println("Sending request: " + requests[1]);
-
-                    msg.send(requester);
-
-                    // Wait for response from server
-                    String reply = requester.recvStr(0).trim();
-                    System.out.println("Received reply: " + reply);
-                }
-
-            }
-            scanner.close();
+        // If there's no arguments print the menu
+        if (args.length == 0) {
+            Menu.showMenu();
+            return;
         }
+
+        boolean hasService = false;
+        boolean hasIsbn = false;
+        boolean hasCampus = false;
+
+        for (int i = 0; i < args.length; i++) {
+
+            // Checks for '-s' flag
+            if (args[i].equals("-s")) {
+                if (i + 1 < args.length) {
+                    service = args[i + 1];
+                    hasService = true;
+                    i++; // Jumps to the next argument
+                } else {
+                    System.out.println("Flag '-s' requires a service value (Borrow, Renewal, or Return).");
+                    return;
+                }
+            }
+            // Checks for '--isbn' flag
+            else if (args[i].equals("--isbn")) {
+                if (i + 1 < args.length) {
+                    isbn = args[i + 1];
+                    hasIsbn = true;
+                    i++; // Jumps to the next argument
+                } else {
+                    System.out.println("Flag '--isbn' requires an ISBN value.");
+                    return;
+                }
+            }
+            // Checks for '-c' flag
+            else if (args[i].equals("-c")) {
+                if (i + 1 < args.length) {
+                    try {
+                        campus = Integer.parseInt(args[i + 1]);
+                        if (campus != 1 && campus != 2) {
+                            System.out.println("The value for '-c' flag should be 1 or 2.");
+                            return;
+                        }
+                        hasCampus = true;
+                        i++; // Jumps to the next argument
+                    } catch (NumberFormatException e) {
+                        System.out.println("The value for '-c' flag must be an Integer.");
+                        return;
+                    }
+                } else {
+                    System.out.println("The flag '-c' requires a campus value.");
+                    return;
+                }
+            }
+            // Checks for '-f' flag
+            else if (args[i].equals("-f")) {
+                if (i + 1 < args.length) {
+                    fileName = args[i + 1];
+                    i++; // Jumps to the next argument
+                } else {
+                    System.out.println("The flag '-f' requires a file name.");
+                    return;
+                }
+            }
+        }
+
+        // Check if at least one of the required flags is present
+        if (!hasService || !hasIsbn || !hasCampus) {
+            System.out.println("Missing one or more required flags. Please provide values for -s, --isbn, and -c.");
+            return;
+        }
+
+        // Call the appropriate operation based on the provided arguments
+        if (service.equalsIgnoreCase("Borrow")) {
+            Operation.sendRequest(Operation.createRequest("Borrow", isbn, Integer.toString(campus)));
+        } else if (service.equalsIgnoreCase("Renewal")) {
+            Operation.sendRequest(Operation.createRequest("Renewal", isbn, Integer.toString(campus)));
+        } else if (service.equalsIgnoreCase("Return")) {
+            // Logic
+
+        } else {
+            System.out.println("Invalid service value. Please use Borrow, Renewal, or Return.");
+        }
+
+        if (!fileName.isEmpty()) {
+            // Logic
+        }
+
     }
 }
