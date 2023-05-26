8fc37f4cfbbb19e6734158fbf9e017dfaf8e0560
Camilo Nossa <calejandro_noss@outlook.com>
2023-05-26 18:53:30 -0500

    New main for the server

--- server/src/main/java/com/server/App.java
+++ server/src/main/java/com/server/App.java
@@ -1,22 +1,65 @@
 package com.server;
 
-import com.server.model.LoadManager;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.server.logic.ProcessLogic;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
+    private static Process loadManagerProcess = ProcessLogic.startProcess("java", "-jar",
+            Dotenv.load().get("LOAD_MANAGER_JAR"));
 
     public static void main(String[] args) throws Exception {
 
+        // Add a shutdown hook to destroy the main process when the server is shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutting down server...");
+        }));
+
         logger.info("Initializing server...");
 
-        LoadManager loadManager = new LoadManager();
+        // Add a shutdown hook to destroy the loadManagerProcess when the server is
+        // shutdown
+        ProcessLogic.addShutdownHook(loadManagerProcess);
 
-        logger.info("Server running for Request-Reply on port 5555");
-        loadManager.receiveRequest();
+        // Wait for the loadManagerProcess to start
+        if (ProcessLogic.waitForProcess(loadManagerProcess) && loadManagerProcess != null) {
+            logger.info("Load Manager started successfully");
+            logger.info("Load Manager PID: " + loadManagerProcess.pid());
+
+        } else {
+            logger.error("Load Manager failed to start");
+            return;
+        }
+
+        // Start a thread to print the output from the loadManagerProcess
+
+        while (true) {
+
+            if (!loadManagerProcess.isAlive()) {
+                logger.error("Load Manager process is not running");
+
+                ProcessLogic.stopProcess(loadManagerProcess);
+
+                logger.info("Attempting to restart Load Manager process...");
+                loadManagerProcess = ProcessLogic.startProcess("java", "-jar",
+                        Dotenv.load().get("LOAD_MANAGER_JAR"));
+                ProcessLogic.addShutdownHook(loadManagerProcess);
+
+                if (ProcessLogic.waitForProcess(loadManagerProcess)) {
+                    logger.info("Load Manager restarted successfully");
+                    logger.info("Load Manager PID: " + loadManagerProcess.pid());
+                } else {
+                    logger.error("Load Manager failed to restart");
+                    return;
+                }
+            }
+
+            Thread.sleep(1000);
+        }
+
     }
 }
-
-
