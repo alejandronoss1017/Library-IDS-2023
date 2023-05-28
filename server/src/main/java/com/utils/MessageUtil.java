d081b19ac0f59ef1aa84d064567c7f6e3e97cba0
Camilo Nossa <calejandro_noss@outlook.com>
2023-05-28 14:20:18 -0500

    New overload of the method sendMessage

--- server/src/main/java/com/utils/MessageUtil.java
+++ server/src/main/java/com/utils/MessageUtil.java
@@ -8,4 +8,14 @@ public class MessageUtil {
     public static void sendMessage(ZMQ.Socket socket, ZMsg msg) {
         msg.send(socket);
     }
+
+    public static void sendMessage(ZMQ.Socket socket, String message) {
+
+        ZMsg msg = new ZMsg();
+
+        msg.add(message);
+
+        msg.send(socket);
+    }
+
 }
