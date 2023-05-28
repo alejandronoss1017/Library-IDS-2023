38fb0a76fa47c30b0f4512c01c8a4345b37ec544
Camilo Nossa <calejandro_noss@outlook.com>
2023-05-28 18:32:18 -0500

    New dbOperation method

--- server/src/main/java/com/actors/DoWorkThread.java
+++ server/src/main/java/com/actors/DoWorkThread.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.zeromq.ZMsg;
 
 import com.messageQueue.MessageQueue;
+import com.server.db.BorrowOperations;
+import com.server.model.Borrow;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class DoWorkThread implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(DoWorkThread.class);
@@ -20,9 +24,10 @@ public class DoWorkThread implements Runnable {
 
             try {
                 ZMsg msg = messageQueue.consume();
+
                 // Do database work
-                logger.info("Doing database stuff...");
-                System.out.println("Message working on: " + msg.toString());
+                dbOperation(msg);
+
             } catch (InterruptedException e) {
                 logger.error("Error: " + e.getMessage());
                 Thread.currentThread().interrupt();
@@ -30,6 +35,29 @@ public class DoWorkThread implements Runnable {
             }
         }
         throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+    public static void dbOperation(ZMsg msg) {
+
+        logger.info("Working on: " + msg.toString());
+
+        String service = msg.popString();
+        String isbn = msg.popString();
+
+        Borrow borrow = BorrowOperations.findBorrowByIsbn(isbn);
+
+        if (borrow != null && service.equals(Dotenv.load().get("RENEWAL_TOPIC"))) {
+            BorrowOperations.renewalFinalDate(borrow);
+            logger.info("Borrow renewed: " + isbn);
+        } else if (borrow != null && service.equals(Dotenv.load().get("RETURN_TOPIC"))) {
+            BorrowOperations.returnBorrow(borrow);
+            logger.info("Book returned: " + isbn);
+        } else {
+            logger.error("Borrow not found: " + isbn);
+
+        }
+
+        msg.clear();
     }
 
 }
