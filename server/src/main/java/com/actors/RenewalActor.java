d011a908bc319a3f40717a89a045b55f46e13468
Camilo Nossa <calejandro_noss@outlook.com>
2023-05-28 15:55:14 -0500

    Re-factorization of actors
    Now both use MessageQueue class

--- server/src/main/java/com/actors/RenewalActor.java
+++ server/src/main/java/com/actors/RenewalActor.java
@@ -7,22 +7,37 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMsg;
 
+import com.messageQueue.MessageQueue;
+import com.utils.SocketUtil;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class RenewalActor {
     private static final Logger logger = LoggerFactory.getLogger(RenewalActor.class);
+    private static final String PUBLISHER_IP = Dotenv.load().get("PUBLISHER_IP");
+    private static final String PUBLISHER_PORT = Dotenv.load().get("PUB_SUB_PORT");
+    private static MessageQueue renewalWorkQueue = MessageQueue.getInstance();
 
     public static void main(String[] args) {
         try (ZContext context = new ZContext()) {
 
-            ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
-            subscriber.connect("tcp://localhost:7777");
+            ZMQ.Socket subscriber = SocketUtil.connectSocket(context, SocketType.SUB, PUBLISHER_IP, PUBLISHER_PORT,
+                    false);
 
             subscriber.subscribe("Renewal");
 
-            logger.info("Renewal listening on port 7777");
+            logger.info("Renewal listening on port" + PUBLISHER_PORT + " ...");
+
+            // Start a thread to do the work of the actor
+            Thread renewalThread = new Thread(new DoWorkThread(renewalWorkQueue));
+            renewalThread.start();
 
             while (!Thread.currentThread().isInterrupted()) {
 
                 ZMsg msg = ZMsg.recvMsg(subscriber);
+
+                // Notify the thread that there is work to do and pass it the message.
+                renewalWorkQueue.produce(msg);
 
                 logger.info("Renewal actor received: " + msg.toString());
             }
--- server/src/main/java/com/actors/ReturnActor.java
+++ server/src/main/java/com/actors/ReturnActor.java
@@ -7,24 +7,39 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMsg;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import com.messageQueue.MessageQueue;
+import com.utils.SocketUtil;
+
 public class ReturnActor {
     private static final Logger logger = LoggerFactory.getLogger(ReturnActor.class);
+    private static final String PUBLISHER_IP = Dotenv.load().get("PUBLISHER_IP");
+    private static final String PUBLISHER_PORT = Dotenv.load().get("PUB_SUB_PORT");
+    private static MessageQueue returnWorkQueue = MessageQueue.getInstance();
 
     public static void main(String[] args) {
         try (ZContext context = new ZContext()) {
 
-            ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
-            subscriber.connect("tcp://localhost:7777");
+            ZMQ.Socket subscriber = SocketUtil.connectSocket(context, SocketType.SUB, PUBLISHER_IP, PUBLISHER_PORT,
+                    false);
 
             subscriber.subscribe("Return");
 
-            logger.info("Renewal listening on port 7777");
+            logger.info("Renewal listening on port " + PUBLISHER_PORT + " ...");
+
+            // Start a thread to do the work of the actor
+            Thread returnThread = new Thread(new DoWorkThread(returnWorkQueue));
+            returnThread.start();
 
             while (!Thread.currentThread().isInterrupted()) {
 
                 ZMsg msg = ZMsg.recvMsg(subscriber);
 
+                returnWorkQueue.produce(msg);
+
                 logger.info("Renewal actor received: " + msg.toString());
+
             }
 
             subscriber.close();
